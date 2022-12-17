7bbde422b7dbd1524483abcfd7cef005d3e3eaa8
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2022-12-17 16:51:44 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+kamus = [" ", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+arr = list(map(str, input().split()))
+for i in range(len(arr)):
+    print(kamus[int(arr[i][0])][len(arr[i])-1], end="")
+
+# 2 55 88 0 44 33 22 2 8
+# 7 2 7 2 0 6 444 66 8 2 0 7 88 555 7777 2
